39f7dc2a38b3957558c95d54bd66f29febff1263
Administrator <Administrator@WIN-FHPN357U6I1>
2023-10-27 19:04:53 +0800

    Add project files.

---
+++ WindowsFormsApp3/MyThreadClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace WindowsFormsApp3
+{
+    internal class MyThreadClass
+    {
+        public static void Thread1()
+        {
+            for (int LoopCount = 0; LoopCount <= 2; LoopCount++)
+            {
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of thread: " + thread.Name + " = " + LoopCount);
+                Thread.Sleep(500);
+            }
+        }
+
+        public static void Thread2()
+        {
+            for (int LoopCount = 0; LoopCount <= 6; LoopCount++)
+            {
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of thread: " + thread.Name + " = " + LoopCount);
+                Thread.Sleep(1500);
+            }
+        }
+    }
+}
